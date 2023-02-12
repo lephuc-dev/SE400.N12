f7b4d5d5ec445d7c14c729129066c2570b000651
TranHoangdbz <71215879+TranHoangdbz@users.noreply.github.com>
2023-02-12 09:37:37 +0700

    commit
    export cluster to files
    export cities of cluster
    export fixture test

---  sys_recomendation/kmeans-clustering.py
+++  sys_recomendation/kmeans-clustering.py
@@ -6,8 +6,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
+from utils.criteria import scores as city_features
 from utils.functions import read_data_from_json
-from utils.criteria import selected_set_3
 
 
 def find_best_nr_of_clusters(X, nr_clusters_to_be_tested):
@@ -75,19 +75,86 @@ def build_clustering(X, nr_clusters_to_be_tested, cities_name):
     return cities_clusters, in_cluster
 
 
+def export_clusters(output_file_path, cities_clusters):
+    clusters_l = []
+    for ind in range(len(cities_clusters)):
+        clusters_l.append({'cluster_id': ind, 'cities': []})
+
+        cities_group = cities_clusters[ind]
+        for city in cities_group:
+            clusters_l[ind]['cities'].append(city)
+
+    with open(output_file_path, 'w') as js:
+        json.dump(clusters_l, js)
+
+    return clusters_l
+
+
+def export_given_cities_clusters(output_file_path, cities_to_be_explored, cities_clusters, in_cluster):
+    with open(output_file_path, 'w') as output_obj:
+        for city in cities_to_be_explored:
+            output_obj.write(f'Analysis for {city} city:\n')
+            cluster_idx = in_cluster[city]
+            output_obj.write(f"{city}'s cluster contains the following cities:\n")
+
+            neighbour_cities = cities_clusters[cluster_idx]
+            for neigh in neighbour_cities:
+                output_obj.write(neigh + '\n')
+
+            output_obj.write('\n')
+
+
+def insert_cluster_id_to_cities_files(file_path, output_file_path, clusters_l):
+    with open(file_path) as js:
+        loaded_json = json.load(js)
+
+        for dictionary in loaded_json:
+            curr_dict = dictionary['fields']
+            city_name = curr_dict['city']
+
+            for cluster_dict in clusters_l:
+                if city_name in cluster_dict['cities']:
+                    dictionary['fields']['id_cluster'] = cluster_dict['cluster_id']
+
+        with open(output_file_path, 'w') as outfile:
+            json.dump(loaded_json, outfile)
+
+
+
+output_fixture_file_path = None
+
+if len(city_features) == 26:
+    data_file_path = './files/analyze-data.json'
+    output_clusters_file_path = './kmeans/files/data-with-all-features/clusters-all-features.json'
+    output_explored_cities_file_path = './kmeans/files/data-with-all-features/explored-cities-for-all-features-data.txt'
+elif len(city_features) == 5:
+    data_file_path = './files/set-1.json'
+    output_clusters_file_path = './kmeans/files/test-1/clusters-test-1.json'
+    output_fixture_file_path = './kmeans/files/test-1/fixture-test-1.json'
+    output_explored_cities_file_path = './kmeans/files/test-1/explored-cities-for-test-1.txt'
+elif len(city_features) == 4:
+    data_file_path = './files/set-2.json'
+    output_clusters_file_path = './kmeans/files/test-2/clusters-test-2.json'
+    output_fixture_file_path = './kmeans/files/test-2/fixture-test-2.json'
+    output_explored_cities_file_path = './kmeans/files/test-2/explored-cities-for-test-2.txt'
+else:
+    data_file_path = './files/set-3.json'
+    output_clusters_file_path = './kmeans/files/test-3/clusters-test-3.json'
+    output_fixture_file_path = './kmeans/files/test-3/fixture-test-3.json'
+    output_explored_cities_file_path = './kmeans/files/test-3/explored-cities-for-test-3.txt'
 
 df, city_names, line_index_in_df = read_data_from_json(
-    file_path="./files/set-3.json",
-    city_features=selected_set_3
+    file_path=data_file_path,
+    city_features=city_features
 )
 
 
 """ Clustering """
 input_data = df.values
 input_data_scaled = MinMaxScaler().fit_transform(input_data)
-print(f'Input data: {input_data}')
-print(f'Input data length: {len(input_data)}')
-print(f'Scaled input data: {input_data_scaled}')
+# print(f'Input data: {input_data}')
+# print(f'Input data length: {len(input_data)}')
+# print(f'Scaled input data: {input_data_scaled}')
 ncl_list = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
 
 clusters, city_in_cluster = build_clustering(
@@ -96,9 +163,31 @@ clusters, city_in_cluster = build_clustering(
     cities_name=city_names
 )
 
+""" List of clusters and their cities """
+clusters_list = export_clusters(
+    output_file_path=output_clusters_file_path,
+    cities_clusters=clusters
+)
+# print(clusters)
 
-print(clusters)
+# print('==========================================')
 
-print('==========================================')
+# print(city_in_cluster)
 
-print(city_in_cluster)
+# Looking at some cities' cluster to see if the other cities in their
+# corresponding cluster are similar to them (checking how accurate the clustering is)
+cities_to_explore = ['phuket', 'london', 'shanghai', 'bangkok', 'dubai', 'sao-paulo']
+export_given_cities_clusters(
+    output_file_path=output_explored_cities_file_path,
+    cities_to_be_explored=cities_to_explore,
+    cities_clusters=clusters,
+    in_cluster=city_in_cluster
+)
+
+if output_fixture_file_path:
+    """ Insert id_cluster for each city in the Json file """
+    insert_cluster_id_to_cities_files(
+        file_path=data_file_path,
+        output_file_path=output_fixture_file_path,
+        clusters_l=clusters_list
+    )
